6b99e6442857bd573c2f071db7dea241f54a45db
MaximoffMq <jose.morales6@catolica.edu.sv>
2025-04-01 20:12:35 -0600

    Prototipo 01 funcional

--- L02P02_2022MQ650_2022DS601/Models/libros.cs
+++ L02P02_2022MQ650_2022DS601/Models/libros.cs
@@ -1,6 +1,17 @@
-﻿namespace L02P02_2022MQ650_2022DS601.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace L02P02_2022MQ650_2022DS601.Models
 {
     public class libros
     {
+        [Key]
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+        public string url_imagen { get; set; }
+        public int id_autor { get; set; }
+        public int id_categoria { get; set; }
+        public decimal precio { get; set; }
+        public char estado { get; set; }
     }
 }
